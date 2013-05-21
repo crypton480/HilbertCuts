6b9813bb76145ce7972108d531f69d72448d6eca
crypton480 <tanmaydesh5886@gmail.com>
2013-05-20 21:17:55 -0700

    Hilbert Bases

---
+++ forloop.cpp
@@ -0,0 +1,16 @@
+//write nested for loops
+//="0";e["<<i<<"]<2;e["<<i<<"]++)\n"
+#include <iostream>  
+#include <fstream>
+using namespace std; 
+		     
+	
+int main()
+{
+int n,i;
+cout<<"\nEnter no of vertices: ";
+cin>>n;
+cout<<"\n";
+for(i=0;i<n;i++)
+cout<<"for(e["<<i<<"]=0;e["<<i<<"]<2;e["<<i<<"]++)\n";
+}
